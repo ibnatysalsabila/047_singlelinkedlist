84683b161668703847bed274aff6e84adad54f92
ibnatysalsabila <salsajpr01@gmail.com>
2024-04-25 18:37:37 +0700

    menambahkan void addNode

--- singellinkedlist/singellinkedlist.cpp
+++ singellinkedlist/singellinkedlist.cpp
@@ -6,3 +6,15 @@ struct Node {
 	string name;
 	Node* next;
 };
+Node* START = NULL;
+
+void addNode() {
+	int nim;
+	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukkan NIM: ";
+	cin >> nim;
+	cout << "Masukkan NAMA: ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
