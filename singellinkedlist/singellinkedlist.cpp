f964d917412d3eec51d5b71e24c5bfb3dfabbe6e
ibnatysalsabila <salsajpr01@gmail.com>
2024-04-25 18:46:17 +0700

    memasukan void traverse

--- singellinkedlist/singellinkedlist.cpp
+++ singellinkedlist/singellinkedlist.cpp
@@ -82,3 +82,20 @@ bool listEmpty() {
 	else
 		return false;
 }
+
+void traverse() {
+	if (listEmpty()) {
+		cout << "List Kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		Node* currentNode = START;
+		while (currentNode != NULL) {
+			cout << "NIM: " << currentNode->noMhs << ", Nama: " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
+}
+
